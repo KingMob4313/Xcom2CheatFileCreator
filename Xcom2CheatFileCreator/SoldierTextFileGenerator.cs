6d497a999a6963468be43fc6a6140a30cec58e47
KingMob4313 <shortstop4313@gmail.com>
2017-03-18 15:14:33 -0400

    Fix

--- Xcom2CheatFileCreator/SoldierTextFileGenerator.cs
+++ Xcom2CheatFileCreator/SoldierTextFileGenerator.cs
@@ -14,7 +14,7 @@ namespace Xcom2CheatFileCreator
         public static List<Soldier> ProcessSoldierFile(List<Soldier> soldierListInstance, string outputFileName, int level, char driveLetter, bool? isLongWar2)
         {
             StringBuilder textFor = new StringBuilder();
-            textFor.AppendLine("; -----Date Generated : " + DateTime.Now.ToShortDateString() + "----");
+            textFor.AppendLine("; ---Date Generated : " + DateTime.Now.ToShortDateString() + "---");
             foreach (Soldier s in soldierListInstance)
             {
                 string composedClassName = string.Empty;
@@ -32,25 +32,25 @@ namespace Xcom2CheatFileCreator
                 {
                     composedClassName = classPrefix + s.SoldierClass.ToString();
                 }
-                textFor.AppendLine("; -----Set Class----");
+                textFor.AppendLine("; ---Set Class---");
                 textFor.AppendLine("MakeSoldierAClass \"" + s.FirstName + " " + s.LastName + "\" " + composedClassName);
-                textFor.AppendLine("; -----General Stats----");
+                textFor.AppendLine("; ---General Stats---");
                 textFor.AppendLine("SetSoldierStat eStat_HP " + s.Health + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_Mobility " + s.Mobility + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_SightRadius " + s.SightRadius + " " + s.FirstName + " " + s.LastName);
-                textFor.AppendLine("; -----Offense Stats----");
+                textFor.AppendLine("; ---Offense Stats---");
                 textFor.AppendLine("SetSoldierStat eStat_Offense " + s.Aim + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_CritChance " + s.CritChance + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_ArmorPiercing " + s.ArmorPierce + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_FlankingCritChance " + s.FlankingCrit + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_FlankingAimBonus " + s.FlankingAim + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_PsiOffense " + s.PsiOffensive + " " + s.FirstName + " " + s.LastName);
-                textFor.AppendLine("; -----Defense Stats----");
+                textFor.AppendLine("; ---Defense Stats---");
                 textFor.AppendLine("SetSoldierStat eStat_Will " + s.Will + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_Dodge " + (Math.Ceiling(s.Dodge * 1.5)).ToString() + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_Defense " + s.Dodge + " " + s.FirstName + " " + s.LastName);
                 textFor.AppendLine("SetSoldierStat eStat_ArmorMitigation " + s.Armor + " " + s.FirstName + " " + s.LastName);
-                textFor.AppendLine("; -----Other Stats----");
+                textFor.AppendLine("; ---Other Stats---");
                 string modifiedHackSkill = (s.SoldierClass == LWClass.Specialist ? (s.Hack * 2) : s.Hack).ToString();
                 textFor.AppendLine("SetSoldierStat eStat_Hacking " + modifiedHackSkill + " " + s.FirstName + " " + s.LastName);
                 //textFor.AppendLine("SetSoldierStat eStat_UtilityItems " + s.UtilitySlots + " " + s.FirstName + " " + s.LastName);
@@ -69,7 +69,7 @@ namespace Xcom2CheatFileCreator
                         ii++;
                     }
                 }
-                textFor.AppendLine("; -----NEXT SOLDIER PLEASE----");
+                textFor.AppendLine("; ---NEXT SOLDIER PLEASE---");
                 textFor.AppendLine(";");
             }
             if (level > 0)
